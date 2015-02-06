9c2900bf429d924ff949e74d5f71db21c07747f4
Romain NICOLAS <rnicolas@e3r4p2.42.fr>
2015-02-06 20:31:37 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   main.cpp                                           :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: rnicolas <rnicolas@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2015/02/06 20:04:27 by rnicolas          #+#    #+#             //
+//   Updated: 2015/02/06 20:16:45 by rnicolas         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+/*
+
+comment exprimer la map
+classe coordonnee, dans un container
+
+generer la map
+genere les coordonnees
+
+comment exprimer un organisme
+suite d'index de coordonnees 
+
+generer des organismes
+evaluer les organismes<--------------------+
+  distance plus court = mieux              |
+brassage genetique == melange les genomes  |
+
+mutations                                  |
+
+-------------------------------------------+
+
+*/
