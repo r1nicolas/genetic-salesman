1e33cdfc196d38611147b0a863d90bc2781f05da
Romain NICOLAS <rnicolas@e3r4p2.42.fr>
2015-02-06 20:37:53 +0100

    Vertex coplien

--- Vertex.class.cpp
+++ Vertex.class.cpp
@@ -6,7 +6,7 @@
 //   By: rnicolas <rnicolas@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/02/06 20:21:56 by rnicolas          #+#    #+#             //
-//   Updated: 2015/02/06 20:28:06 by rnicolas         ###   ########.fr       //
+//   Updated: 2015/02/06 20:37:04 by rnicolas         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -17,11 +17,26 @@ Vertex::Vertex() : _x(0), _y(0)
 {
 }
 
-Vertex::Vertex(double _x, double _y): _x(x), _y(y)
+Vertex::Vertex(double x, double y): _x(x), _y(y)
 {
 }
 
-double	distance_to(Vertex const &v) const
+Vertex::Vertex(Vertex &ori): _x(ori._x), _y(ori._y)
+{
+}
+
+Vertex::~Vertex()
+{
+}
+
+Vertex	&Vertex::operator=(Vertex &rhs)
+{
+	this->_x = rhs._x;
+	this->_y = rhs._y;
+	return (*this);
+}
+
+double	Vertex::distance_to(Vertex const &v) const
 {
 	return (sqrt(pow(this->_x - v._x, 2) +  pow(this->_y - v._y, 2)));
 }
--- Vertex.class.hpp
+++ Vertex.class.hpp
@@ -6,7 +6,7 @@
 //   By: rnicolas <rnicolas@student.42.fr>          +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/02/06 20:18:23 by rnicolas          #+#    #+#             //
-//   Updated: 2015/02/06 20:24:12 by rnicolas         ###   ########.fr       //
+//   Updated: 2015/02/06 20:36:45 by rnicolas         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -18,6 +18,10 @@ private:
 public:
 	Vertex();
 	Vertex(double x, double y);
+	Vertex(Vertex &ori);
+	~Vertex();
+
+	Vertex	&operator=(Vertex &rhs);
 
 	double	distance_to(Vertex const &v) const;
-}
+};
