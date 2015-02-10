df3715bc89c4122b018bd8b60f153446745e0a6c
Romain NICOLAS <rnicolas@e3r3p5.42.fr>
2015-02-10 18:13:15 +0100

    vertex

--- Vertex.class.cpp
+++ Vertex.class.cpp
@@ -13,30 +13,33 @@
 #include <cmath>
 #include "Vertex.class.hpp"
 
-Vertex::Vertex() : _x(0), _y(0)
-{
+Vertex::Vertex() : _x(0), _y(0) {
+
 }
 
-Vertex::Vertex(double x, double y): _x(x), _y(y)
-{
+Vertex::Vertex(double x, double y): _x(x), _y(y) {
+
 }
 
-Vertex::Vertex(Vertex const &ori): _x(ori._x), _y(ori._y)
-{
+Vertex::Vertex(Vertex const &ori): _x(ori._x), _y(ori._y) {
+
 }
 
-Vertex::~Vertex()
-{
+Vertex::~Vertex() {
+
 }
 
-Vertex	&Vertex::operator=(Vertex const &rhs)
-{
+Vertex	&Vertex::operator=(Vertex const &rhs) {
+
 	this->_x = rhs._x;
 	this->_y = rhs._y;
+
 	return (*this);
+
 }
 
-double	Vertex::distance_to(Vertex const &v) const
-{
+double	Vertex::distance_to(Vertex const &v) const {
+
 	return (sqrt(pow(this->_x - v._x, 2) +  pow(this->_y - v._y, 2)));
+
 }
